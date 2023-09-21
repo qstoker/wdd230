4a42d7f4cf4d5e88ec82b249b1f068e3e07e9925
qstoker <quinnpstoker@gmail.com>
2023-09-20 21:56:45 -0700

    Fix dark mode link contrast

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,14 +1,25 @@
 const modeButton = document.querySelector("#mode");
 const body = document.querySelector("body");
+const cardLinks = document.querySelector(".card").querySelectorAll("a");
 
 modeButton.addEventListener("click", () => {
   if (modeButton.textContent.includes("☽")) {
     body.style.background = "#202124";
     body.style.color = "#fff";
     modeButton.textContent = "☼";
+
+    cardLinks.forEach((link) => {
+      link.style.textDecoration = "underline";
+      link.style.color = "#81c3d7";
+    });
   } else {
     body.style.background = "#fff";
     body.style.color = "#000";
     modeButton.textContent = "☽";
+
+    cardLinks.forEach((link) => {
+      link.style.textDecoration = "underline";
+      link.style.color = "#2f6690";
+    });
   }
 });
