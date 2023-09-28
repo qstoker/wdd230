7f6fc7b62c7469c7647c885236bad68c1db79c18
qstoker <quinnpstoker@gmail.com>
2023-09-27 20:48:58 -0700

    Edit

---
+++ scripts/page-visits.js
@@ -0,0 +1,11 @@
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+  visitsDisplay.textContent = numVisits;
+} else {
+  visitsDisplay.textContent = `0`;
+}
+
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
