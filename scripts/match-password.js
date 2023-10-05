4c9727a5387caedadb753fbd8a3875681984f46f
qstoker <quinnpstoker@gmail.com>
2023-10-05 14:02:48 -0700

    Edit contrast

--- scripts/match-password.js
+++ scripts/match-password.js
@@ -7,13 +7,15 @@ p2.addEventListener("focusout", checkSame);
 function checkSame() {
   if (p1.value !== p2.value) {
     message.textContent = "Passwords do not match";
+    message.style.color = "#d33b3b";
     p2.style.color = "#000000";
     p2.style.backgroundColor = "#ffd8d8";
     p1.value = "";
     p2.value = "";
     p1.focus();
   } else {
-    message.textContent = "";
+    message.textContent = "No errors to report";
+    message.style.color = "#3a823a";
     p2.style.color = "#000000";
     p2.style.backgroundColor = "#ffffff";
   }
