d286f6d16117996be52961611dc04d01141b666c
qstoker <quinnpstoker@gmail.com>
2023-10-04 22:10:16 -0700

    Edit

--- scripts/match-password.js
+++ scripts/match-password.js
@@ -1,21 +1,18 @@
 const p1 = document.querySelector("#password");
 const p2 = document.querySelector("#password-confirmation");
-const message = document.querySelector("#formmessage");
+const message = document.querySelector("#form-message");
 
 p2.addEventListener("focusout", checkSame);
 
-// This should be refactored.
 function checkSame() {
   if (p1.value !== p2.value) {
-    message.textContent = "❗Key Phrases DO NOT MATCH!";
-    // message.style.visibility = "show";
-    p2.style.backgroundColor = "#fff0f3";
+    message.textContent = "Passwords do not match";
+    p2.style.backgroundColor = "#ffd8d8";
     p1.value = "";
     p2.value = "";
     p1.focus();
   } else {
-    message.style.display = "none";
-    p2.style.backgroundColor = "#fff";
-    p2.style.color = "#000";
+    message.textContent = "";
+    p2.style.backgroundColor = "#ffffff";
   }
 }
