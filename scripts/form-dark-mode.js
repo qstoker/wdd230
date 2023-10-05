12a8da3479fe3a378de97c8e97f235d41c9d82b6
qstoker <quinnpstoker@gmail.com>
2023-10-05 11:17:41 -0700

    Apply dark mode to fields

--- scripts/form-dark-mode.js
+++ scripts/form-dark-mode.js
@@ -1,14 +1,25 @@
 const modeButton = document.querySelector("#mode");
 const body = document.querySelector("body");
+const inputFields = document.querySelector("fieldset").querySelectorAll("input");
 
 modeButton.addEventListener("click", () => {
   if (modeButton.textContent.includes("☽")) {
     body.style.background = "#202124";
     body.style.color = "#fff";
     modeButton.textContent = "☼";
+
+    inputFields.forEach((input) => {
+      input.style.background = "#202124";
+      input.style.color = "#fff";
+    });
   } else {
     body.style.background = "#fff";
     body.style.color = "#000";
     modeButton.textContent = "☽";
+
+    inputFields.forEach((input) => {
+      input.style.background = "#fff";
+      input.style.color = "#000";
+    });
   }
 });
