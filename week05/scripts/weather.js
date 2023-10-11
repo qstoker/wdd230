6a3be2a754d9fe9177f1a51c2313ec57bbbc1d59
qstoker <quinnpstoker@gmail.com>
2023-10-10 17:58:29 -0700

    Initial commit

---
+++ week05/scripts/weather.js
@@ -0,0 +1,32 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=65a9eae82689d459b9b05fdeffb14b1d";
+
+async function apiFetch() {
+  try {
+    let response = await fetch(url);
+    if (response.ok) {
+      let data = await response.json();
+
+      console.log(data);
+
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
