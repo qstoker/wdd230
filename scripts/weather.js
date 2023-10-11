dc1ec855e4d71d68d32e3f1fc09c64706347fec6
qstoker <quinnpstoker@gmail.com>
2023-10-10 19:29:57 -0700

    Edit weather

---
+++ scripts/weather.js
@@ -0,0 +1,35 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const description = document.querySelector("#weather-description");
+const place = document.querySelector("#weather-location");
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=47.72&lon=-116.95&units=imperial&appid=65a9eae82689d459b9b05fdeffb14b1d";
+
+async function apiFetch() {
+  try {
+    let response = await fetch(url);
+    if (response.ok) {
+      let data = await response.json();
+
+      console.log(data);
+
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  let loc = data.name;
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  description.textContent = `${desc}`;
+  place.textContent = `${loc}`;
+}
+
+apiFetch();
