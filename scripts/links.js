191697895ed0f8d63cf36658cf2b70c4823dcb43
qstoker <quinnpstoker@gmail.com>
2023-10-12 10:17:38 -0700

    Finish json links assignment

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,40 @@
 const baseURL = "https://qstoker.github.io/wdd230/";
 const linksURL = "https://qstoker.github.io/wdd230/data/links.json";
+const weeksList = document.querySelector(".weeks-list");
+
+async function getLinks() {
+  const response = await fetch(linksURL);
+  const data = await response.json();
+
+  displayLinks(data.weeks);
+}
+
+function displayLinks(weeks) {
+  weeks.forEach((week) => {
+    let newWeek = document.createElement("li");
+    // let weekName = document.createElement("span");
+    newWeek.textContent = `${week.week}`;
+
+    week.links.forEach((link) => {
+      let spacer = document.createElement("span");
+      let url = document.createElement("a");
+
+      if (link == week.links[0]) {
+        spacer.textContent = `: `;
+      } else {
+        spacer.textContent = ` | `;
+      }
+      url.setAttribute("href", `${link.url}`);
+      url.textContent = `${link.title}`;
+
+      if (url.textContent !== "") {
+        newWeek.appendChild(spacer);
+      }
+      newWeek.appendChild(url);
+    });
+
+    weeksList.appendChild(newWeek);
+  });
+}
+
+getLinks();
