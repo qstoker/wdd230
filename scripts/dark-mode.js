30615467ed2337afdc8f91f1d041b8133233df96
qstoker <quinnpstoker@gmail.com>
2023-10-12 11:17:58 -0700

    Simplify dark mode

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,25 +1,14 @@
 const modeButton = document.querySelector("#mode");
 const body = document.querySelector("body");
-const cardLinks = document.querySelector(".card").querySelectorAll("a");
+const a = document.querySelector("a");
 
 modeButton.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  body.classList.toggle("a");
+
   if (modeButton.textContent.includes("☽")) {
-    body.style.background = "#202124";
-    body.style.color = "#fff";
     modeButton.textContent = "☼";
-
-    cardLinks.forEach((link) => {
-      link.style.textDecoration = "underline";
-      link.style.color = "#81c3d7";
-    });
   } else {
-    body.style.background = "#fff";
-    body.style.color = "#000";
     modeButton.textContent = "☽";
-
-    cardLinks.forEach((link) => {
-      link.style.textDecoration = "underline";
-      link.style.color = "#2f6690";
-    });
   }
 });
