e7664ea36a94a68d27869a35c12279682dc0abc9
qstoker <quinnpstoker@gmail.com>
2023-10-22 21:03:33 -0700

    Add banner based on day of the week]

---
+++ chamber/scripts/banner.js
@@ -0,0 +1,19 @@
+const today = new Date();
+const dayOfWeek = today.getDay();
+const banner = document.querySelector("#banner");
+
+if (dayOfWeek == 1 || dayOfWeek == 2 || dayOfWeek == 3) {
+  let text = document.createElement("p");
+  let closeBanner = document.createElement("div");
+
+  text.textContent = "Join us for for a meet and greet at 7:00 p.m. on Wednesday!";
+
+  closeBanner.id = "close-banner";
+
+  banner.appendChild(text);
+  banner.appendChild(closeBanner);
+
+  closeBanner.addEventListener("click", () => {
+    banner.classList.toggle("close");
+  });
+}
